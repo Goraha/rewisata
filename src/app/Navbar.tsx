8cef539037a3d01173a28a5c4c4f7fd1ffc62ba2
GA-Purba <gabrielandrianpurba@gmail.com>
2024-06-08 00:32:08 +0700

    rework navbar & add react icons

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,9 +1,35 @@
+"use client";
+import { FaWindows,FaList } from "react-icons/fa6";
+import { useState } from "react";
 export default function Navbar() {
+  const [navbar, setNavbar] = useState(false);
   return (
-    <nav className="flex bg-black text-white p-5">
-      <ul>
-        <li>Home</li>
-      </ul>
-    </nav>
+    <nav className="flex items-center justify-between flex-wrap p-4 bg-gradient-to-r from-violet-900 via-sky-600 to-sky-500">
+    <div className="flex items-center flex-shrink-0 text-white mr-6">
+      <FaWindows />
+      <span className="font-semibold text-xl tracking-tight">ReWisata</span>
+    </div>
+    <div className="block lg:hidden">
+      <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white" onClick={() => setNavbar(!navbar)}>
+      <FaList />
+      </button>
+    </div>
+    <div className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${navbar ? "block" : "hidden"}`}>
+      <div className="text-sm lg:flex-grow">
+        <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          Beranda
+        </a>
+        <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          Tentang
+        </a>
+        <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+          Cara
+        </a>
+      </div>
+      <div>
+        <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a>
+      </div>
+    </div>
+  </nav>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
 
 import Map from "./Map";
-
+import { FaMagnifyingGlass  } from "react-icons/fa6";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-5">
+      
+      <div className="w-full h-fit p-5 border-2 my-5 rounded">
+        <div className="w-full">
+          <div className="relative flex rounded-lg shadow-sm">
+            <input type="text" className="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-s-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" />
+            <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
+            <FaMagnifyingGlass  />
+            </div>
+            <button type="button" className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-e-md border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">Cari</button>
+          </div>
+        </div>
+
+        <div className="w-full h-96 my-5">
+          <Map />
+        </div>
+      </div>
+
       <div className="w-full h-96 bg-red-500">
 
-      </div>
-      <div className="w-full h-screen">
-      <Map />
       </div>
     </main>
   );
